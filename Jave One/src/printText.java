ad91e80945c65b0c9393cdb3e9ae54bee403d6ee
prashant <prashant.jamdade-ext@jci.com>
2018-08-01 15:10:54 +0530

    commit print

---
+++ Jave One/src/printText.java
@@ -0,0 +1,11 @@
+
+public class printText {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Save Print
+		// save Dir
+		System.out.println("Hello Boss");
+	}
+
+}
